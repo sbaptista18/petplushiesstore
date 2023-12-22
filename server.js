a3a3e3d026aac123c8ec1123814e9414af2438ad
Sílvia Baptista <silviabaptista18@gmail.com>
2023-12-22 19:42:49 +0000

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const WooCommerceAPI = require("react-native-woocommerce-api");
-const PORT = process.env.PORT;
+const PORT = 8000;
 require("dotenv").config();
 
 const app = express();
