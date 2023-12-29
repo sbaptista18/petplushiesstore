7014db3da2c29abdee5fc22eae5f6097f5757923
Sílvia Baptista <silviabaptista18@gmail.com>
2023-12-29 19:15:33 +0000

    update recaptcha sitekey

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -231,7 +231,8 @@ const ContactUs = () => {
                     extra="Só queremos ter a certeza que é um humano."
                   >
                     <ReCAPTCHA
-                      sitekey="6LeeeyEpAAAAAHEmtDr81K8xOhEkbCcM32FGYqtF"
+                      // sitekey="6LeeeyEpAAAAAHEmtDr81K8xOhEkbCcM32FGYqtF" //localhost
+                      sitekey="6LcGYz8pAAAAAKL8E_B9V_DkFqIXiApSnLnfE4Z0"
                       onChange={handleVerification}
                     />
                   </Form.Item>
